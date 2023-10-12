6a000b4e4931ff165ce15747dbff9c743a1b40a6
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-12 15:07:08 +0200

    Initial commit.I have added the code meant to calculate my weight on Mars in comparison to my code on Earth.

---
+++ src/ProjectClass.java
@@ -0,0 +1,16 @@
+public class ProjectClass {
+    public static void main(String[] args) {
+
+        float myWeightEarthNhlanhla = 60f;
+        /*This is the calculation of my weight on Mars by taking 38% of my Earth weight*/
+        double myWeightMarsNhlanhla  = (myWeightEarthNhlanhla * 38)/100;
+
+
+
+
+
+
+
+
+    }
+}
