b3d0ca7d91449c94f5c8b81c34deb02fd89ec8c9
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-18 20:53:41 +0200

    I have now added and improved the calculation of the ascii value of my variable plus 50. Now it displays the integer value of the character H.

--- src/ProjectClass.java
+++ src/ProjectClass.java
@@ -5,9 +5,11 @@ public class ProjectClass {
         /*This is the calculation of my weight on Mars by taking 38% of my Earth weight */
         float myWeightMarsNhlanhla  = ( myWeightEarthNhlanhla * 38)/100;
         double myWeightMarsInDouble = myWeightMarsNhlanhla;
-        int myWeightMarsInInt = (int)myWeightMarsNhlanhla;
+        int myWeightMarsInInt = (int) myWeightMarsInDouble;
         char myWeightMarsInChar = (char)myWeightMarsInInt;
-        char calculation = (char) (myWeightMarsInChar + '5'+'a');
+        char CharacterResult = (char) (myWeightMarsInInt + 50d);
+        int CharacterResultInt = (int) CharacterResult;
+
 
 
 
@@ -15,9 +17,9 @@ public class ProjectClass {
         System.out.printf("%.1f kg on Earth is %2f kg on Mars",myWeightEarthNhlanhla,myWeightMarsNhlanhla);
         System.out.printf("\nKilograms on Mars after I convert it to double is %f",myWeightMarsInDouble);
         System.out.printf("\nKilograms on Mars when displayed to four decimal places is %.4f",myWeightMarsNhlanhla);
-        System.out.printf("\nKilograms on Mars when casted to integer is %d",myWeightMarsInInt);
+        System.out.printf("\nKilograms on Mars when casted to integer is " + myWeightMarsInInt);
         System.out.printf("\nThe ASCII table equivalent of marsWeightInInt is %c",myWeightMarsInChar);
-        System.out.printf("\nAn example of a mathematical operation on the char type is:%c",calculation);
+        System.out.printf("\nAn example of a mathematical operation on the char type is "+ CharacterResultInt);
 
 
 
